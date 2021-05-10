a39e02964140c8e0f8f140e9d7b27bd6f43ce165
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-10 11:02:57 -0700

    adding BuildingDecorationRepositoryImpl.java.

---
+++ src/main/java/ir/maktab/data/repository/BuildingDecoration/BuildingDecorationRepositoryImpl.java
@@ -0,0 +1,71 @@
+package ir.maktab.data.repository.BuildingDecoration;
+
+import ir.maktab.data.entity.BuildingDecoration.BuildingDecoration;
+import ir.maktab.data.entity.BuildingDecoration.SubBuildingDecoration;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BuildingDecorationRepositoryImpl implements BuildingDecorationRepository{
+    private final SessionFactory sessionFactory;
+
+    public BuildingDecorationRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void create(BuildingDecoration buildingDecoration) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(buildingDecoration);
+        tx.commit();
+        session.close();
+    }
+
+    @Override
+    public void update(BuildingDecoration buildingDecoration) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(buildingDecoration);
+        tx.commit();
+        session.close();
+
+    }
+
+    @Override
+    public Optional<BuildingDecoration> get(Integer id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        javax.persistence.Query query  = session.createQuery("from ir.maktab.data.entity.BuildingDecoration as c  where c.id = :c_id")
+                .setParameter("c_id",id);
+        Optional f =  query.getResultList().stream().findFirst();
+        transaction.commit();
+        session.close();
+        return f;
+    }
+
+    @Override
+    public List<BuildingDecoration> getAll() {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        List<BuildingDecoration> list =
+                (List<BuildingDecoration>) session.createQuery("from ir.maktab.data.entity.BuildingDecoration").list();
+        tx.commit();
+        session.close();
+        return list;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.createQuery("delete from ir.maktab.data.entity.BuildingDecoration as c  where c.id = :c_id")
+                .setParameter("c_id",id).executeUpdate();
+        tx.commit();
+        session.close();
+
+    }
+}
