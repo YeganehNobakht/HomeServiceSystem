86c40befc2c6362c54011cf29038bab695de2c89
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-10 00:56:31 -0700

    adding SubHomeAppliances.java

---
+++ src/main/java/ir/maktab/data/entity/homeAppliances/SubHomeAppliances.java
@@ -0,0 +1,63 @@
+package ir.maktab.data.entity.homeAppliances;
+
+import ir.maktab.data.entity.CleaningAndHygiene.CleaningAndHygiene;
+
+import javax.persistence.*;
+
+@Entity
+public class SubHomeAppliances {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double price;
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name="home_appliances", nullable=false)
+    private HomeAppliances homeAppliances;
+
+    public SubHomeAppliances() {
+    }
+
+    public SubHomeAppliances(Integer id, Double price, String comment, HomeAppliances homeAppliances) {
+        this.id = id;
+        this.price = price;
+        this.comment = comment;
+        this.homeAppliances = homeAppliances;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public SubHomeAppliances setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public SubHomeAppliances setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public SubHomeAppliances setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public HomeAppliances getHomeAppliances() {
+        return homeAppliances;
+    }
+
+    public SubHomeAppliances setHomeAppliances(HomeAppliances homeAppliances) {
+        this.homeAppliances = homeAppliances;
+        return this;
+    }
+}
