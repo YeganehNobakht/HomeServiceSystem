d9c3911872301e789a7951cc6a5e267b6e66c0e8
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-10 00:28:36 -0700

    adding sub class to BuildingFacilities.java

---
+++ src/main/java/ir/maktab/data/entity/BuildingFacilities/SubBuildingFacilities.java
@@ -0,0 +1,65 @@
+package ir.maktab.data.entity.BuildingFacilities;
+
+import javax.persistence.Entity;
+
+import ir.maktab.data.entity.BuildingDecoration.BuildingDecoration;
+
+import javax.persistence.*;
+
+@Entity
+public class SubBuildingFacilities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double price;
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name="building_decoration", nullable=false)
+    private BuildingFacilities buildingFacilities;
+
+    public SubBuildingFacilities() {
+    }
+
+    public SubBuildingFacilities(Integer id, Double price, String comment, BuildingFacilities buildingFacilities) {
+        this.id = id;
+        this.price = price;
+        this.comment = comment;
+        this.buildingFacilities = buildingFacilities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public SubBuildingFacilities setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public SubBuildingFacilities setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public SubBuildingFacilities setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public BuildingFacilities getBuildingFacilities() {
+        return buildingFacilities;
+    }
+
+    public SubBuildingFacilities setBuildingFacilities(BuildingFacilities buildingFacilities) {
+        this.buildingFacilities = buildingFacilities;
+        return this;
+    }
+}
