6a025ed8358c15fe123d7d3a1921083d126095ab
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-10 01:01:12 -0700

    adding SubVehicles.java

---
+++ src/main/java/ir/maktab/data/entity/vehicle/SubVehicle.java
@@ -0,0 +1,63 @@
+package ir.maktab.data.entity.vehicle;
+
+import ir.maktab.data.entity.homeAppliances.HomeAppliances;
+
+import javax.persistence.*;
+
+@Entity
+public class SubVehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double price;
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name="vehicles", nullable=false)
+    private Vehicles vehicles;
+
+    public SubVehicle() {
+    }
+
+    public SubVehicle(Integer id, Double price, String comment, Vehicles vehicles) {
+        this.id = id;
+        this.price = price;
+        this.comment = comment;
+        this.vehicles = vehicles;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public SubVehicle setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public SubVehicle setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public SubVehicle setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public Vehicles getVehicles() {
+        return vehicles;
+    }
+
+    public SubVehicle setVehicles(Vehicles vehicles) {
+        this.vehicles = vehicles;
+        return this;
+    }
+}
