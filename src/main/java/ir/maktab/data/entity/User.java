c42431b04cf90e0da6c91bec39481a5363e43655
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-14 23:18:31 -0700

    adding User.java super class.

---
+++ src/main/java/ir/maktab/data/entity/User.java
@@ -0,0 +1,97 @@
+package ir.maktab.data.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public class User {
+    @Id
+    private String username;
+
+    private String password;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated
+    private UserStatus userStatus;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    private Double Balance;
+
+    public User(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserStatus getUserStatus() {
+        return userStatus;
+    }
+
+    public User setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public User setDate(Date date) {
+        this.date = date;
+        return this;
+    }
+
+    public Double getBalance() {
+        return Balance;
+    }
+
+    public User setBalance(Double balance) {
+        Balance = balance;
+        return this;
+    }
+}
