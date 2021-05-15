f4e243ba69f9b9c691a310c1d7b11fd1cb62c3c6
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-14 23:19:26 -0700

    adding parent class to child class.

--- src/main/java/ir/maktab/data/entity/Customer.java
+++ src/main/java/ir/maktab/data/entity/Customer.java
@@ -6,91 +6,32 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Customer {
-    @Id
-    private String username;
+public class Customer extends User {
 
-    private String fullName;
-
-    @Column(unique = true)
-    private String email;
 
     @Enumerated
-    private Status status;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date date;
-
-    private Double Balance;
+    private OrderStatus orderStatus;
 
     @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST, mappedBy = "customer", fetch = FetchType.EAGER)
     private final List<CustomerOrder> customerOrderList = new ArrayList<>();
 
-
-    public Customer(String username, String fullName, String email) {
-        this.username = username;
-        this.fullName = fullName;
-        this.email = email;
+    public Customer(String username, String password, String fullName) {
+        super(username, password, fullName);
     }
 
     public Customer() {
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public Customer setUsername(String username) {
-        this.username = username;
-        return this;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public Customer setFullName(String fullName) {
-        this.fullName = fullName;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Customer setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public Customer setStatus(Status status) {
-        this.status = status;
-        return this;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Customer setDate(Date date) {
-        this.date = date;
-        return this;
-    }
-
-    public Double getBalance() {
-        return Balance;
-    }
-
-    public Customer setBalance(Double balance) {
-        Balance = balance;
-        return this;
-    }
-
     public List<CustomerOrder> getCustomerOrderList() {
         return customerOrderList;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public Customer setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+        return this;
     }
 }
--- src/main/java/ir/maktab/data/entity/Specialist.java
+++ src/main/java/ir/maktab/data/entity/Specialist.java
@@ -2,28 +2,10 @@ package ir.maktab.data.entity;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Specialist {
-    @Id
-    private String username;
-
-    private String password;
-
-    private String fullName;
-
-    @Column(unique = true)
-    private String email;
-
-    @Enumerated
-    private Status status;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date date;
-
-    private Double Balance;
+public class Specialist extends User{
 
     @Lob
     @Basic(fetch = FetchType.LAZY)
@@ -35,67 +17,14 @@ public class Specialist {
     @OneToMany(orphanRemoval = true , cascade = CascadeType.PERSIST , mappedBy = "serviceCategory" , fetch = FetchType.EAGER)
     private List<CustomerOrder> customerOrderList = new ArrayList<>();
 
-    public String getUsername() {
-        return username;
+
+   //constructor
+    public Specialist(String username, String password, String fullName) {
+        super(username, password, fullName);
     }
 
-    public Specialist setUsername(String username) {
-        this.username = username;
-        return this;
-    }
+    public Specialist(){
 
-    public String getPassword() {
-        return password;
-    }
-
-    public Specialist setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public Specialist setFullName(String fullName) {
-        this.fullName = fullName;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Specialist setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public Specialist setStatus(Status status) {
-        this.status = status;
-        return this;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Specialist setDate(Date date) {
-        this.date = date;
-        return this;
-    }
-
-    public Double getBalance() {
-        return Balance;
-    }
-
-    public Specialist setBalance(Double balance) {
-        Balance = balance;
-        return this;
     }
 
     public byte[] getProfilePicture() {
