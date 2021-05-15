4b8c3c103d09ce24c751f4c99d5c46a48e343df5
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-15 00:06:30 -0700

    adding ServiceCategoryRepository.java and ServiceCategoryRepositoryImpl.java.

---
+++ src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepository.java
@@ -0,0 +1,15 @@
+package ir.maktab.data.repository.serviceCategory;
+
+import ir.maktab.data.entity.CustomerOrder;
+import ir.maktab.data.entity.ServiceCategory;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ServiceCategoryRepository {
+    void create(ServiceCategory serviceCategory);
+    void update(ServiceCategory serviceCategory);
+    Optional<ServiceCategory> get(Integer id);
+    List<ServiceCategory> getAll();
+    void delete(Integer integer);
+}
---
+++ src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepositoryImpl.java
@@ -0,0 +1,73 @@
+package ir.maktab.data.repository.serviceCategory;
+
+import ir.maktab.data.entity.CustomerOrder;
+import ir.maktab.data.entity.ServiceCategory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ServiceCategoryRepositoryImpl implements ServiceCategoryRepository {
+
+
+    private final SessionFactory sessionFactory;
+
+    public ServiceCategoryRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void create(ServiceCategory serviceCategory) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(serviceCategory);
+        tx.commit();
+        session.close();
+    }
+
+    @Override
+    public void update(ServiceCategory serviceCategory) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(serviceCategory);
+        tx.commit();
+        session.close();
+
+    }
+
+    @Override
+    public Optional<ServiceCategory> get(Integer id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        javax.persistence.Query query  = session.createQuery("from ir.maktab.data.entity.ServiceCategory as c  where c.id = :c_id")
+                .setParameter("c_id",id);
+        Optional f =  query.getResultList().stream().findFirst();
+        transaction.commit();
+        session.close();
+        return f;
+    }
+
+    @Override
+    public List<ServiceCategory> getAll() {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        List<ServiceCategory> list =
+                (List<ServiceCategory>) session.createQuery("from ir.maktab.data.entity.ServiceCategory").list();
+        tx.commit();
+        session.close();
+        return list;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.createQuery("delete from ir.maktab.data.entity.ServiceCategory as c  where c.id = :c_id")
+                .setParameter("c_id",id).executeUpdate();
+        tx.commit();
+        session.close();
+
+    }
+}
