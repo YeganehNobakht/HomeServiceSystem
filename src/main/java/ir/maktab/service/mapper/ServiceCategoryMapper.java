ad01494f9e65f74027f73ad177f97a311fa7bc59
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-15 21:20:02 -0700

    adding ServiceCategoryMapper.

---
+++ src/main/java/ir/maktab/service/mapper/ServiceCategoryMapper.java
@@ -0,0 +1,21 @@
+package ir.maktab.service.mapper;
+
+import ir.maktab.data.entity.ServiceCategory;
+import ir.maktab.dto.ServiceCategoryDto;
+
+public class ServiceCategoryMapper {
+    public ServiceCategory toServiceCategory(ServiceCategoryDto serviceCategoryDto){
+        return new ServiceCategory()
+                .setId(serviceCategoryDto.getId())
+                .setName(serviceCategoryDto.getName())
+                .setSubCategoryList(serviceCategoryDto.getSubCategoryList())
+                .setCustomerOrderList(serviceCategoryDto.getCustomerOrderList());
+    }
+    public ServiceCategoryDto toServiceCategoryDto(ServiceCategory serviceCategory){
+        return new ServiceCategoryDto()
+                .setId(serviceCategory.getId())
+                .setName(serviceCategory.getName())
+                .setSubCategoryList(serviceCategory.getSubCategoryList())
+                .setCustomerOrderList(serviceCategory.getCustomerOrderList());
+    }
+}
