33aec9c742614576a8d0ba97c77b9d3c2313f9d5
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-15 21:58:29 -0700

    adding methods.

--- src/main/java/ir/maktab/service/validations/Validations.java
+++ src/main/java/ir/maktab/service/validations/Validations.java
@@ -2,5 +2,11 @@ package ir.maktab.service.validations;
 
 public interface Validations {
 
-    boolean validatePassword(String password);
+    boolean validatePassword(String password) throws Exception;
+
+    boolean validateEmail(String email) throws Exception;
+
+    boolean validateName(String name) throws Exception;
+
+    boolean validateUsername(String username) throws Exception;
 }
--- src/main/java/ir/maktab/service/validations/validationsImpl.java
+++ src/main/java/ir/maktab/service/validations/validationsImpl.java
@@ -2,16 +2,46 @@ package ir.maktab.service.validations;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class validationsImpl implements Validations{
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern VALID_PASSWORD_REGEX = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$");
+    private static final Pattern VALID_FULL_NAME_REGEX =  Pattern.compile("^[a-zA-Z\\s]+");
+    private static final Pattern VALID_USERNAME_REGEX =  Pattern.compile("[a-zA-Z0-9\\._\\-]{3,}");
 
 
     @Override
-    public boolean validatePassword(String password) {
-        String regex ="^(?=.*[0-9])"
-                + "(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher matcher = p.matcher(password);
-        return matcher.matches();
+    public boolean validatePassword(String password) throws Exception {
+        Matcher matcher = VALID_PASSWORD_REGEX.matcher(password);
+        if (matcher.matches())
+            return true;
+        else
+            throw new Exception("Password id too weak.");
+    }
+    @Override
+    public boolean validateEmail(String email) throws Exception {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        if (matcher.matches())
+            return true;
+        else
+            throw new Exception("Wrong email format.");
+    }
+    @Override
+    public boolean validateName(String name) throws Exception {
+        Matcher matcher = VALID_FULL_NAME_REGEX.matcher(name);
+        if (matcher.matches())
+            return true;
+        else
+            throw new Exception("Wrong name format");
+    }
+    @Override
+    public boolean validateUsername(String username) throws Exception {
+        Matcher matcher = VALID_USERNAME_REGEX.matcher(username);
+        if (matcher.matches())
+            return true;
+        else
+            throw new Exception("The password must be at least 3 characters and a combination of letters and numbers.");
     }
 }
