3e3d03fbdaf7480dd0c6866a247879f52bbd1f78
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-18 21:31:55 -0700

    fixing constructor.

--- src/main/java/ir/maktab/data/entity/Customer.java
+++ src/main/java/ir/maktab/data/entity/Customer.java
@@ -18,8 +18,8 @@ public class Customer extends User {
     private final List<CustomerComment> customerCommentList = new ArrayList<>();
 
 
-    public Customer(String username, String password, String fullName, String email) {
-        super(username, password, fullName, email);
+    public Customer(String username, String password, String name, String lastName, String email) {
+        super(username, password, name, lastName, email);
     }
 
     public Customer() {
