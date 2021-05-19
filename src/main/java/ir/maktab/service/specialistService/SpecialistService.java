a7eb9d8b75a57812b50bbc033aab2f1816ad21f4
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-18 21:42:55 -0700

    adding change password method.

--- src/main/java/ir/maktab/service/specialistService/SpecialistService.java
+++ src/main/java/ir/maktab/service/specialistService/SpecialistService.java
@@ -3,7 +3,10 @@ package ir.maktab.service.specialistService;
 import ir.maktab.data.entity.Specialist;
 
 public interface SpecialistService {
+
     void create(Specialist specialist);
 
     void delete(String specialistUsername);
+
+    public void changePassword(String username, String oldPass,String newPass) throws Exception;
 }
--- src/main/java/ir/maktab/service/specialistService/SpecialistServiceImpl.java
+++ src/main/java/ir/maktab/service/specialistService/SpecialistServiceImpl.java
@@ -2,14 +2,22 @@ package ir.maktab.service.specialistService;
 
 import ir.maktab.data.entity.Specialist;
 import ir.maktab.data.repository.specialist.SpecialistRepository;
+import ir.maktab.service.validations.Validations;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class SpecialistServiceImpl implements SpecialistService{
 
     private final SpecialistRepository specialistRepository;
+    private final Validations validations;
 
-    public SpecialistServiceImpl(SpecialistRepository specialistRepository) {
+    public SpecialistServiceImpl(SpecialistRepository specialistRepository, Validations validations) {
         this.specialistRepository = specialistRepository;
+        this.validations = validations;
     }
+
 
     @Override
     public void create(Specialist specialist) {
@@ -19,5 +27,18 @@ public class SpecialistServiceImpl implements SpecialistService{
     @Override
     public void delete(String specialistUsername) {
         specialistRepository.delete(specialistUsername);
+    }
+
+    @Override
+    public void changePassword(String username, String oldPass,String newPass) throws Exception {
+        Optional<Specialist> specialist = specialistRepository.get(username);
+        if (specialist.isPresent()) {
+            if (specialist.get().getPassword().equals(oldPass)) {
+                if (validations.validatePassword(newPass)) {
+                    specialist.get().setPassword(newPass);
+                    specialistRepository.update(specialist.get());
+                }
+            }
+        }
     }
 }
