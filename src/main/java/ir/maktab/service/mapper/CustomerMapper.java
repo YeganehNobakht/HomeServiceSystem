42a9307a958fdcea03e976c8d3d57d55088168d3
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-20 04:52:37 -0700

    adding CustomerMapper.

---
+++ src/main/java/ir/maktab/service/mapper/CustomerMapper.java
@@ -0,0 +1,36 @@
+package ir.maktab.service.mapper;
+
+import ir.maktab.data.entity.Customer;
+import ir.maktab.data.entity.Specialist;
+import ir.maktab.dto.CustomerDto;
+import ir.maktab.dto.SpecialistDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+    public Customer toCustomer(CustomerDto customerDto){
+        return (Customer) new Customer()
+                .setBalance(customerDto.getBalance())
+                .setDate(customerDto.getDate())
+                .setEmail(customerDto.getEmail())
+                .setName(customerDto.getName())
+                .setLastName(customerDto.getLastName())
+                .setPassword(customerDto.getPassword())
+                .setUsername(customerDto.getUsername())
+                .setUserStatus(customerDto.getUserStatus());
+
+    }
+    public CustomerDto toCustomerDto(Customer customer){
+        return (CustomerDto) new CustomerDto()
+                .setBalance(customer.getBalance())
+                .setDate(customer.getDate())
+                .setEmail(customer.getEmail())
+                .setName(customer.getName())
+                .setLastName(customer.getLastName())
+                .setPassword(customer.getPassword())
+                .setUsername(customer.getUsername())
+                .setUserStatus(customer.getUserStatus());
+
+    }
+
+}
