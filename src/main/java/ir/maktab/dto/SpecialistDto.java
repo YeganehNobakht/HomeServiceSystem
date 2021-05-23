4532d1249c7b539adbdf62cd0bb706f8465ff8cd
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-23 01:05:32 -0700

    override setters.

--- src/main/java/ir/maktab/dto/SpecialistDto.java
+++ src/main/java/ir/maktab/dto/SpecialistDto.java
@@ -3,11 +3,13 @@ package ir.maktab.dto;
 import ir.maktab.data.entity.CustomerComment;
 import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.entity.ServiceCategory;
+import ir.maktab.data.entity.enums.UserStatus;
 
 import javax.persistence.CascadeType;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SpecialistDto extends UserDto{
@@ -48,6 +50,60 @@ public class SpecialistDto extends UserDto{
 
     public SpecialistDto setServiceCategoryList(List<ServiceCategory> serviceCategoryList) {
         this.serviceCategoryList = serviceCategoryList;
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setUsername(String username) {
+        super.setUsername(username);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setPassword(String password) {
+        super.setPassword(password);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setName(String name) {
+        super.setName(name);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setLastName(String lastName) {
+        super.setLastName(lastName);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setEmail(String email) {
+        super.setEmail(email);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setUserStatus(UserStatus userStatus) {
+        super.setUserStatus(userStatus);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setDate(Date date) {
+        super.setDate(date);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setUserRole(String userRole) {
+        super.setUserRole(userRole);
+        return this;
+    }
+
+    @Override
+    public SpecialistDto setBalance(Double balance) {
+        super.setBalance(balance);
         return this;
     }
 }
