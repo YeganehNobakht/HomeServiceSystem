cda65cf839f81f37d0e6d05eca5f366a69dd3426
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-23 01:05:05 -0700

    override setters.

--- src/main/java/ir/maktab/data/entity/Customer.java
+++ src/main/java/ir/maktab/data/entity/Customer.java
@@ -1,9 +1,12 @@
 package ir.maktab.data.entity;
 
 import ir.maktab.data.entity.enums.OrderStatus;
+import ir.maktab.data.entity.enums.UserStatus;
+import ir.maktab.dto.CustomerDto;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -31,5 +34,54 @@ public class Customer extends User {
 
     public List<CustomerComment> getCustomerCommentList() {
         return customerCommentList;
+    }
+
+
+    @Override
+    public Customer setUsername(String username) {
+        super.setUsername(username);
+        return this;
+    }
+
+    @Override
+    public Customer setPassword(String password) {
+        super.setPassword(password);
+        return this;
+    }
+
+    @Override
+    public Customer setName(String name) {
+        super.setName(name);
+        return this;
+    }
+
+    @Override
+    public Customer setLastName(String lastName) {
+        super.setLastName(lastName);
+        return this;
+    }
+
+    @Override
+    public Customer setEmail(String email) {
+        super.setEmail(email);
+        return this;
+    }
+
+    @Override
+    public Customer setUserStatus(UserStatus userStatus) {
+        super.setUserStatus(userStatus);
+        return this;
+    }
+
+    @Override
+    public Customer setDate(Date date) {
+        super.setDate(date);
+        return this;
+    }
+
+    @Override
+    public Customer setBalance(Double balance) {
+        super.setBalance(balance);
+        return this;
     }
 }
