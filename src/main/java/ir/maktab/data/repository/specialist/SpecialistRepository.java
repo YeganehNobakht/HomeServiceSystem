cbcf907dd416ae7aa1f702e0c9e3990d1822c1b3
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:51:54 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
+++ src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
@@ -3,15 +3,17 @@ package ir.maktab.data.repository.specialist;
 import ir.maktab.data.entity.Customer;
 import ir.maktab.data.entity.Specialist;
 import ir.maktab.dto.SpecialistDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface SpecialistRepository {
-    void create(Specialist specialist);
-    void update(Specialist specialist);
-    Optional<Specialist> get(String id);
-    List<Specialist> getAll();
-    void delete(String  username);
-    List<SpecialistDto> filterSpecialist(SpecialistDto specialistDto);
+@Repository
+public interface SpecialistRepository extends JpaRepository<Customer,String> {
+//    void create(Specialist specialist);
+//    void update(Specialist specialist);
+//    Optional<Specialist> get(String id);
+//    List<Specialist> getAll();
+//    void delete(String  username);
+//    List<SpecialistDto> filterSpecialist(SpecialistDto specialistDto);
 }
