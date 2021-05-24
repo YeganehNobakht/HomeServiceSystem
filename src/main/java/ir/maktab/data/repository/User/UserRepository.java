aa1eda042e18e4cb27b81ac6312ef02c4edbe429
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:59:28 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/User/UserRepository.java
+++ src/main/java/ir/maktab/data/repository/User/UserRepository.java
@@ -1,10 +1,13 @@
 package ir.maktab.data.repository.User;
 
+import ir.maktab.data.entity.User;
 import ir.maktab.dto.UserDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
 
-public interface UserRepository {
-
-    List<UserDto> filterUser(UserDto userDto);
+//    List<UserDto> filterUser(UserDto userDto);
 }
