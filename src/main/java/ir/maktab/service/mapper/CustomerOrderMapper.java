83091c857e5181600590543e741b7ee4f8777b04
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:15:45 -0700

    adding CustomerOrderDto.

---
+++ src/main/java/ir/maktab/service/mapper/CustomerOrderMapper.java
@@ -0,0 +1,32 @@
+package ir.maktab.service.mapper;
+
+import ir.maktab.data.entity.CustomerOrder;
+import ir.maktab.dto.CustomerOrderDto;
+
+public class CustomerOrderMapper {
+    public CustomerOrder toCustomerOrder(CustomerOrderDto customerOrderDto){
+        return new CustomerOrder()
+                .setAddress(customerOrderDto.getAddress())
+                .setCustomer(customerOrderDto.getCustomer())
+                .setOrderDate(customerOrderDto.getOrderDate())
+                .setOrderStatus(customerOrderDto.getOrderStatus())
+                .setServiceCategory(customerOrderDto.getServiceCategory())
+                .setSubCategory(customerOrderDto.getSubCategory())
+                .setId(customerOrderDto.getId())
+                .setJobDescription(customerOrderDto.getJobDescription())
+                .setWorkDate(customerOrderDto.getWorkDate());
+    }
+
+    public CustomerOrderDto toCustomerOrderDto(CustomerOrder customerOrder) {
+        return new CustomerOrderDto()
+                .setAddress(customerOrder.getAddress())
+                .setCustomer(customerOrder.getCustomer())
+                .setOrderDate(customerOrder.getOrderDate())
+                .setOrderStatus(customerOrder.getOrderStatus())
+                .setServiceCategory(customerOrder.getServiceCategory())
+                .setSubCategory(customerOrder.getSubCategory())
+                .setId(customerOrder.getId())
+                .setJobDescription(customerOrder.getJobDescription())
+                .setWorkDate(customerOrder.getWorkDate());
+    }
+}
