4d8ad6c0a6b7e2b7c94b96ae790575776e755548
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:58:16 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/subCategory/SubCategoryRepository.java
+++ src/main/java/ir/maktab/data/repository/subCategory/SubCategoryRepository.java
@@ -2,15 +2,17 @@ package ir.maktab.data.repository.subCategory;
 
 import ir.maktab.data.entity.ServiceCategory;
 import ir.maktab.data.entity.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface SubCategoryRepository {
-    void create(SubCategory subCategory);
-    void update(SubCategory subCategory);
-    Optional<SubCategory> get(Integer id);
-    Optional<SubCategory> getByName(String name);
-    List<SubCategory> getAll();
-    void delete(Integer integer);
+@Repository
+public interface SubCategoryRepository extends JpaRepository<SubCategory,Integer> {
+//    void create(SubCategory subCategory);
+//    void update(SubCategory subCategory);
+//    Optional<SubCategory> get(Integer id);
+//    Optional<SubCategory> getByName(String name);
+//    List<SubCategory> getAll();
+//    void delete(Integer integer);
 }
