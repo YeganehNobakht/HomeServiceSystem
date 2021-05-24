5c08147dbf50462d8182b6ec2d623589eb9f6eb7
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:17:45 -0700

    changing method signature.

--- src/main/java/ir/maktab/service/customerService/CustomerService.java
+++ src/main/java/ir/maktab/service/customerService/CustomerService.java
@@ -1,14 +1,16 @@
 package ir.maktab.service.customerService;
 
 import ir.maktab.data.entity.Customer;
+import ir.maktab.dto.CustomerDto;
+import ir.maktab.dto.CustomerOrderDto;
 
 public interface CustomerService {
 
     Customer customerSignIn() throws Exception;
 
-    void addOrder(Customer customer) throws Exception;
+    void addOrder(CustomerOrderDto customerDto) ;
 
     void changePassword(String username, String oldPass,String newPass) throws Exception;
 
-    void create(Customer customer);
+    void create(CustomerDto customerOrderDto) throws Exception;
 }
