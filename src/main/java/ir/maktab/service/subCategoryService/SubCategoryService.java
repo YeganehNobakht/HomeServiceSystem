9802e0d8f7c436a3386a9277e84ff30487d20d57
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:49:45 -0700

    adding method.

--- src/main/java/ir/maktab/service/subCategoryService/SubCategoryService.java
+++ src/main/java/ir/maktab/service/subCategoryService/SubCategoryService.java
@@ -1,8 +1,11 @@
 package ir.maktab.service.subCategoryService;
 
 import ir.maktab.data.entity.SubCategory;
+import ir.maktab.dto.ServiceCategoryDto;
+import ir.maktab.dto.SubCategoryDto;
 
 public interface SubCategoryService {
     SubCategory get(Integer id) throws Exception;
     SubCategory getByName(String  name) throws Exception;
+    void addSubService(ServiceCategoryDto serviceCategoryDto, SubCategoryDto subCategoryDto) throws Exception;
 }
--- src/main/java/ir/maktab/service/subCategoryService/SubCategoryServiceImpl.java
+++ src/main/java/ir/maktab/service/subCategoryService/SubCategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package ir.maktab.service.subCategoryService;
 
+import ir.maktab.data.entity.ServiceCategory;
 import ir.maktab.data.entity.SubCategory;
 import ir.maktab.data.repository.subCategory.SubCategoryRepository;
+import ir.maktab.dto.ServiceCategoryDto;
+import ir.maktab.dto.SubCategoryDto;
+import ir.maktab.service.mapper.SubCategoryMapper;
+import ir.maktab.service.serviceCategory.ServiceCategoryService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,11 +15,15 @@ import java.util.Optional;
 public class SubCategoryServiceImpl implements SubCategoryService{
 
     private final SubCategoryRepository subCategoryRepository;
+    private final ServiceCategoryService serviceCategoryService;
+    private final SubCategoryMapper subCategoryMapper;
 
-    public SubCategoryServiceImpl(SubCategoryRepository subCategoryRepository) {
+
+    public SubCategoryServiceImpl(SubCategoryRepository subCategoryRepository, ServiceCategoryService serviceCategoryService, SubCategoryMapper subCategoryMapper) {
         this.subCategoryRepository = subCategoryRepository;
+        this.serviceCategoryService = serviceCategoryService;
+        this.subCategoryMapper = subCategoryMapper;
     }
-
 
     @Override
     public SubCategory get(Integer id) throws Exception {
@@ -32,5 +41,12 @@ public class SubCategoryServiceImpl implements SubCategoryService{
             return subCategory.get();
         else
             throw new Exception("Service does not exist");
+    }
+
+    @Override
+    public void addSubService(ServiceCategoryDto serviceCategoryDto, SubCategoryDto subCategoryDto) throws Exception {
+        ServiceCategory serviceByName = serviceCategoryService.getByName(serviceCategoryDto.getName());
+        subCategoryDto.setServiceCategory(serviceByName);
+        subCategoryRepository.update(subCategoryMapper.toSubCategory(subCategoryDto));
     }
 }
