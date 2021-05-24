3d247b105022b3796fc868c1ff1d2540fc916d77
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:16:04 -0700

    adding CustomerOrderMapper.

--- src/main/java/ir/maktab/service/customerOrderService/CustomerOrderService.java
+++ src/main/java/ir/maktab/service/customerOrderService/CustomerOrderService.java
@@ -1,7 +1,12 @@
 package ir.maktab.service.customerOrderService;
 
 import ir.maktab.data.entity.CustomerOrder;
+import ir.maktab.dto.CustomerDto;
+import ir.maktab.dto.CustomerOrderDto;
+import ir.maktab.dto.ServiceCategoryDto;
+import ir.maktab.dto.SubCategoryDto;
 
 public interface CustomerOrderService {
-    void create(CustomerOrder customerOrder);
+    void addOrder(CustomerOrderDto customerOrderDto);
+
 }
