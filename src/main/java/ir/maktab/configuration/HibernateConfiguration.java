d686975f9da7279e73d3426c9889bc3b15839cd1
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 06:45:52 -0700

    adding HibernateConfiguration.

--- src/main/java/ir/maktab/configuration/HibernateConfiguration.java
+++ src/main/java/ir/maktab/configuration/HibernateConfiguration.java
@@ -1,47 +1,58 @@
 package ir.maktab.configuration;
 
-import ir.maktab.data.entity.Customer;
-import ir.maktab.data.entity.CustomerOrder;
-import ir.maktab.data.entity.Manager;
-import ir.maktab.data.entity.Specialist;
+import ir.maktab.data.entity.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.util.Properties;
-import java.util.Scanner;
 
+@Configuration
+@PropertySource("classpath:database.properties")
 public class HibernateConfiguration {
+
+    private final Environment environment;
+
+    public HibernateConfiguration(Environment environment) {
+        this.environment = environment;
+    }
 
 
     @Bean("sessionFactory")
-    public SessionFactory getSessionFactory(){
+    public SessionFactory getSessionFactory() {
         try {
             org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
             configuration.addAnnotatedClass(Manager.class);
             configuration.addAnnotatedClass(Specialist.class);
             configuration.addAnnotatedClass(Customer.class);
             configuration.addAnnotatedClass(CustomerOrder.class);
-            configuration.setProperties(new Properties(){
+            configuration.addAnnotatedClass(ServiceCategory.class);
+            configuration.addAnnotatedClass(SubCategory.class);
+            configuration.addAnnotatedClass(CustomerComment.class);
+            configuration.setProperties(new Properties() {
                 {
-                    put("hibernate.connection.driver_class","com.mysql.jdbc.Driver");
-                    put("hibernate.connection.url","jdbc:mysql://localhost:3306/home_service");
-                    put("hibernate.connection.username","root");
-                    put("hibernate.connection.password","root");
-                    put("hibernate.hbm2ddl.auto","update");
-                    put("hibernate.show_sql","true");
+                    put("hibernate.connection.driver_class", environment.getRequiredProperty("hibernate.connection.driver_class"));
+                    put("hibernate.connection.url", environment.getRequiredProperty("hibernate.connection.url"));
+                    put("hibernate.connection.username", environment.getRequiredProperty("hibernate.connection.username"));
+                    put("hibernate.connection.password", environment.getRequiredProperty("hibernate.connection.password"));
+                    put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+                    put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+                    put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+                    put("hibernate.connection.isolation", environment.getRequiredProperty("hibernate.connection.isolation"));
                 }
 
             });
 
             return configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
 }
 
