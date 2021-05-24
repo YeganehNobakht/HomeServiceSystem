804492ece69504c32ee01b5913e5bc38e7407a2d
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:22:24 -0700

    changing method signature.

--- src/main/java/ir/maktab/service/customerOrderService/CustomerOrderServiceImpl.java
+++ src/main/java/ir/maktab/service/customerOrderService/CustomerOrderServiceImpl.java
@@ -1,20 +1,28 @@
 package ir.maktab.service.customerOrderService;
 
-import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.repository.customerOrder.CustomerOrderRepository;
+import ir.maktab.dto.CustomerDto;
+import ir.maktab.dto.CustomerOrderDto;
+import ir.maktab.dto.ServiceCategoryDto;
+import ir.maktab.dto.SubCategoryDto;
+import ir.maktab.service.mapper.CustomerOrderMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerOrderServiceImpl implements CustomerOrderService{
 
     private final CustomerOrderRepository customerOrderRepository;
+    private final CustomerOrderMapper customerOrderMapper;
 
-    public CustomerOrderServiceImpl(CustomerOrderRepository customerOrderRepository) {
+    public CustomerOrderServiceImpl(CustomerOrderRepository customerOrderRepository, CustomerOrderMapper customerOrderMapper) {
         this.customerOrderRepository = customerOrderRepository;
+        this.customerOrderMapper = customerOrderMapper;
     }
+
 
     @Override
-    public void create(CustomerOrder customerOrder) {
-        customerOrderRepository.create(customerOrder);
+    public void addOrder(CustomerOrderDto customerOrderDto) {
+        customerOrderRepository.create(customerOrderMapper.toCustomerOrder(customerOrderDto));
     }
+
 }
