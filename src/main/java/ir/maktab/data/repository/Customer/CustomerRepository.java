fdbf89d215f59138c2e8e5559871e028fdec7eed
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:40:26 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/Customer/CustomerRepository.java
+++ src/main/java/ir/maktab/data/repository/Customer/CustomerRepository.java
@@ -2,15 +2,17 @@ package ir.maktab.data.repository.Customer;
 
 import ir.maktab.data.entity.Customer;
 import ir.maktab.dto.CustomerDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface CustomerRepository {
-    void create(Customer customer);
-    void update(Customer customer);
-    Optional<Customer> get(String  id);
-    List<Customer> getAll();
-    void delete(String integer);
-    List<CustomerDto> filterCustomer(CustomerDto customerDto);
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,String> {
+//    void create(Customer customer);
+//    void update(Customer customer);
+//    Optional<Customer> get(String  id);
+//    List<Customer> getAll();
+//    void delete(String integer);
+//    List<CustomerDto> filterCustomer(CustomerDto customerDto);
 }
