b756472159b656a751d38ad300d433807cd27b72
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:19:39 -0700

    import spring data jpa configuration.

--- src/main/java/ir/maktab/configuration/AppConfiguration.java
+++ src/main/java/ir/maktab/configuration/AppConfiguration.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 @Configuration
 @ComponentScan("ir.maktab")
 @PropertySource("classpath:database.properties")
-@Import(HibernateConfiguration.class)
+@Import(SpringContextDataJPA.class)
 public class AppConfiguration {
 
 //    @Bean("scanner")
