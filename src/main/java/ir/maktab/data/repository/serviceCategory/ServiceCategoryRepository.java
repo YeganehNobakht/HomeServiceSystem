24671932b15cf7a4c8d78b711d0134c4c2f750f1
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:56:57 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepository.java
+++ src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepository.java
@@ -2,15 +2,17 @@ package ir.maktab.data.repository.serviceCategory;
 
 import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.entity.ServiceCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface ServiceCategoryRepository {
-    void create(ServiceCategory serviceCategory);
-    void update(ServiceCategory serviceCategory);
-    Optional<ServiceCategory> get(Integer id);
-    Optional<ServiceCategory> getByName(String name);
-    List<ServiceCategory> getAll();
-    void delete(Integer integer);
+@Repository
+public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory,Integer> {
+//    void create(ServiceCategory serviceCategory);
+//    void update(ServiceCategory serviceCategory);
+//    Optional<ServiceCategory> get(Integer id);
+//    Optional<ServiceCategory> getByName(String name);
+//    List<ServiceCategory> getAll();
+//    void delete(Integer integer);
 }
