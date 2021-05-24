ae48446cf320f4ed3acad8f155bda5689f82cece
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 06:46:29 -0700

    adding AppConfiguration.

--- src/main/java/ir/maktab/configuration/AppConfiguration.java
+++ src/main/java/ir/maktab/configuration/AppConfiguration.java
@@ -1,20 +1,19 @@
 package ir.maktab.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 import java.util.Scanner;
 
+
 @Configuration
 @ComponentScan("ir.maktab")
+@PropertySource("classpath:database.properties")
 @Import(HibernateConfiguration.class)
 public class AppConfiguration {
 
-    @Bean("scanner")
-    public Scanner scanner(){
-        return new Scanner(System.in);
-    }
+//    @Bean("scanner")
+//    public Scanner scanner(){
+//        return new Scanner(System.in);
+//    }
 
 }
