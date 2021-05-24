65c55d5f02ad623f4283f23b6563ab12ffeeaa0e
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 07:46:54 -0700

    implement some methods.

--- src/main/java/ir/maktab/service/serviceCategory/ServiceCategoryServiceImpl.java
+++ src/main/java/ir/maktab/service/serviceCategory/ServiceCategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package ir.maktab.service.serviceCategory;
 
 import ir.maktab.data.entity.ServiceCategory;
+import ir.maktab.data.entity.Specialist;
 import ir.maktab.data.repository.serviceCategory.ServiceCategoryRepository;
+import ir.maktab.dto.ServiceCategoryDto;
+import ir.maktab.dto.SpecialistDto;
+import ir.maktab.service.mapper.ServiceCategoryMapper;
+import ir.maktab.service.mapper.SpecialistMapper;
+import ir.maktab.service.specialistService.SpecialistService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +17,15 @@ import java.util.Optional;
 public class ServiceCategoryServiceImpl implements ServiceCategoryService {
 
     private final ServiceCategoryRepository serviceCategoryRepository;
+    private final SpecialistMapper specialistMapper;
+    private final ServiceCategoryMapper serviceCategoryMapper;
+    private final SpecialistService specialistService;
 
-    public ServiceCategoryServiceImpl(ServiceCategoryRepository serviceCategoryRepository) {
+    public ServiceCategoryServiceImpl(ServiceCategoryRepository serviceCategoryRepository, SpecialistMapper specialistMapper, ServiceCategoryMapper serviceCategoryMapper, SpecialistService specialistService) {
         this.serviceCategoryRepository = serviceCategoryRepository;
+        this.specialistMapper = specialistMapper;
+        this.serviceCategoryMapper = serviceCategoryMapper;
+        this.specialistService = specialistService;
     }
 
     @Override
@@ -38,4 +50,62 @@ public class ServiceCategoryServiceImpl implements ServiceCategoryService {
         else
             throw new Exception("Service does not exist");
     }
+
+    @Override
+    public void addServiceCategory(ServiceCategoryDto serviceCategoryDto) throws Exception {
+        if (serviceCategoryRepository.getByName(serviceCategoryDto.getName()).isPresent()){
+            throw new Exception("Duplicate service");
+        }
+        else {
+            serviceCategoryRepository.create(serviceCategoryMapper.toServiceCategory(serviceCategoryDto));
+        }
+    }
+
+    @Override
+    public void updateSpecialist(SpecialistDto specialistDto) throws Exception {
+
+    }
+
+    @Override
+    public void deleteSpecialist(ServiceCategoryDto  serviceCategoryDto, SpecialistDto specialistDto) throws Exception {
+        Specialist specialist = specialistMapper.toSpecialist(specialistDto);
+        ServiceCategory serviceCategory = getByName(serviceCategoryDto.getName());
+
+        serviceCategory.getSpecialistList().remove(specialist);
+        serviceCategoryRepository.update(serviceCategory);
+
+        specialist.getServiceCategoryList().remove(serviceCategory);
+        specialistService.update(specialistMapper.toSpecialistDto(specialist));
+    }
+
+    @Override
+    public void addSpecialist(ServiceCategoryDto  serviceCategoryDto, SpecialistDto specialistDto) throws Exception {
+
+        Specialist specialist = specialistMapper.toSpecialist(specialistDto);
+        ServiceCategory serviceCategory = getByName(serviceCategoryDto.getName());
+
+        serviceCategory.getSpecialistList().add(specialist);
+        serviceCategoryRepository.update(serviceCategory);
+
+        specialist.getServiceCategoryList().add(serviceCategory);
+        specialistService.update(specialistMapper.toSpecialistDto(specialist));
+    }
+
+//    @Override
+//    public void updateSpecialist(ServiceCategoryDto serviceCategoryDto, SpecialistDto specialistDto) throws Exception {
+////        ServiceCategory serviceCategory = serviceCategoryMapper.toServiceCategory(serviceCategoryDto);
+////        Specialist specialist = specialistMapper.toSpecialist(specialistDto);
+////        if (serviceCategory.getSpecialistList().stream().anyMatch(specialist1 -> specialist1.getUsername().equals(specialist.getUsername()))){
+////
+////        }
+//        if (specialistService.get(specialistDto.getUsername())!=null){
+//            specialistService.update(specialistDto);
+//        }
+//        specialistService.update(specialistDto);
+//
+//    }
+
+
+
+
 }
