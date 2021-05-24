c5f0c123135af3626448c2e981a24ee85ddd09b3
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 08:56:39 -0700

    extending from JpaRepository.

--- src/main/java/ir/maktab/data/repository/customerOrder/CustomerOrderRepository.java
+++ src/main/java/ir/maktab/data/repository/customerOrder/CustomerOrderRepository.java
@@ -1,14 +1,16 @@
 package ir.maktab.data.repository.customerOrder;
 
 import ir.maktab.data.entity.CustomerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
-public interface CustomerOrderRepository {
-    void create(CustomerOrder customerOrder);
-    void update(CustomerOrder customerOrder);
-    Optional<CustomerOrder> get(Integer id);
-    List<CustomerOrder> getAll();
-    void delete(Integer integer);
+@Repository
+public interface CustomerOrderRepository extends JpaRepository<CustomerOrder,Long> {
+//    void create(CustomerOrder customerOrder);
+//    void update(CustomerOrder customerOrder);
+//    Optional<CustomerOrder> get(Integer id);
+//    List<CustomerOrder> getAll();
+//    void delete(Integer integer);
 }
