027b1f7b54e9cd1122b6b7499f9b51edc1bb4e05
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 20:33:44 -0700

    extending from JpaRepository.

---
+++ src/main/java/ir/maktab/data/repository/suggestion/SuggestionRepository.java
@@ -0,0 +1,8 @@
+package ir.maktab.data.repository.suggestion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SuggestionRepository extends JpaRepository {
+}
