6c0e8447d0656334bf2ef923063244f40f4624bd
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-24 20:11:15 -0700

    adding spring data jpa.

--- src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
+++ src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 @Repository
-public interface SpecialistRepository extends JpaRepository<Customer,String> {
+public interface SpecialistRepository extends JpaRepository<Specialist,String> {
 //    void create(Specialist specialist);
 //    void update(Specialist specialist);
 //    Optional<Specialist> get(String id);
