4b91fbd467ae7eeb41790f3c13015d2a92f905b2
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-30 03:24:38 -0700

    adding WebInitializer.

---
+++ src/main/java/ir/maktab/configuration/WebInitializer.java
@@ -0,0 +1,23 @@
+package ir.maktab.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Yeganeh Nobakht
+ **/
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
