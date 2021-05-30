55be8faba71fd95bcd7fa81f9c306a8dd2555b07
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-30 01:40:48 -0700

    adding CustomerCommentDto.

---
+++ src/main/java/ir/maktab/dto/CustomerCommentDto.java
@@ -0,0 +1,62 @@
+package ir.maktab.dto;
+
+import ir.maktab.data.entity.Customer;
+import ir.maktab.data.entity.Specialist;
+
+
+public class CustomerCommentDto {
+    private Integer id;
+
+    private String comment;
+
+    private Double score;
+
+    private CustomerDto customerDto;
+
+    private SpecialistDto specialistDto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public CustomerCommentDto setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public CustomerCommentDto setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public CustomerCommentDto setScore(Double score) {
+        this.score = score;
+        return this;
+    }
+
+    public CustomerDto getCustomerDto() {
+        return customerDto;
+    }
+
+    public CustomerCommentDto setCustomerDto(CustomerDto customerDto) {
+        this.customerDto = customerDto;
+        return this;
+    }
+
+    public SpecialistDto getSpecialistDto() {
+        return specialistDto;
+    }
+
+    public CustomerCommentDto setSpecialistDto(SpecialistDto specialistDto) {
+        this.specialistDto = specialistDto;
+        return this;
+    }
+}
