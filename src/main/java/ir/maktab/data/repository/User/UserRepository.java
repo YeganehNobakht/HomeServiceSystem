6692eb5b1ac0586daa56eceb0655bb1bc8557de6
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-05-30 01:38:30 -0700

    adding specification.

--- src/main/java/ir/maktab/data/repository/User/UserRepository.java
+++ src/main/java/ir/maktab/data/repository/User/UserRepository.java
@@ -1,13 +1,50 @@
 package ir.maktab.data.repository.User;
 
+import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.entity.User;
 import ir.maktab.dto.UserDto;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 @Repository
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
 
 //    List<UserDto> filterUser(UserDto userDto);
+
+    static Specification<User> filterUser(User user){
+        return (Specification<User>) (root, cq, cb) -> {
+            ArrayList<Predicate> predicates = new ArrayList<>();
+
+            //join<>
+           // paganle.....sort(specialistRate)
+
+
+//root:suggestion
+            //Join<suggestion, CustomerOrder> order = root.join("order");
+            //Join<suggestion, specialist> specialist = root.join("specialist");
+
+
+            // cq.orderBy(cb.desc(root.get("price")))
+
+           // cq.orderBy(cb.desc(specialist.get("specialistRate")))
+            if (user.getName()!=null)
+                predicates.add(cb.equal(root.get("name"),user.getName()));
+            if (user.getLastName()!=null)
+                predicates.add(cb.equal(root.get("lastName"),user.getLastName()));
+            if (user.getUserRole()!=null)
+                predicates.add(cb.equal(root.get("userRole"),user.getUserRole()));
+            if (user.getEmail()!=null)
+                predicates.add(cb.equal(root.get("email"),user.getEmail()));
+            return cq.where(predicates.toArray(new Predicate[0])).getRestriction();
+        };
+    }
 }
--- src/main/java/ir/maktab/data/repository/suggestion/SuggestionRepository.java
+++ src/main/java/ir/maktab/data/repository/suggestion/SuggestionRepository.java
@@ -1,8 +1,37 @@
 package ir.maktab.data.repository.suggestion;
 
+import ir.maktab.data.entity.CustomerOrder;
+import ir.maktab.data.entity.Specialist;
+import ir.maktab.data.entity.Suggestion;
+import ir.maktab.data.entity.User;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+
 @Repository
-public interface SuggestionRepository extends JpaRepository {
+public interface SuggestionRepository extends JpaRepository , JpaSpecificationExecutor<Suggestion> {
+
+
+    static Specification<Suggestion> filterSuggestion(Suggestion suggestion){
+        return (Specification<Suggestion>) (root, cq, cb) -> {
+            ArrayList<Predicate> predicates = new ArrayList<>();
+
+            Join<Suggestion, CustomerOrder> order = root.join("customerOrder");
+            Join<Suggestion, Specialist> specialist = root.join("specialist");
+
+
+
+//             cq.orderBy(cb.desc(root.get("price")),);
+//             cq.orderBy(cb.desc(specialist.get("rate")));
+
+            cq.orderBy(cb.desc(root.get("price")),cb.desc(specialist.get("rate")));
+
+            return cq.where(predicates.toArray(new Predicate[0])).getRestriction();
+        };
+    }
 }
