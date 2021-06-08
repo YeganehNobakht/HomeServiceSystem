44f5a52b3473ab5c8b48b57aa619651c19e7621b
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-08 13:02:39 +0430

    changing methods.

--- src/main/java/ir/maktab/service/mapper/SuggestionMapper.java
+++ src/main/java/ir/maktab/service/mapper/SuggestionMapper.java
@@ -2,26 +2,34 @@ package ir.maktab.service.mapper;
 
 import ir.maktab.data.entity.Suggestion;
 import ir.maktab.dto.SuggestionDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SuggestionMapper {
+
+    private final Mapper mapper;
+
+    public SuggestionMapper(Mapper mapper) {
+        this.mapper = mapper;
+    }
 
     Suggestion toSuggestion(SuggestionDto suggestionDto){
         return new Suggestion()
-                .setCustomerOrder(suggestionDto.getCustomerOrder())
+                .setCustomerOrder(mapper.toCustomerOrder(suggestionDto.getCustomerOrder()))
                 .setDurationOfWork(suggestionDto.getDurationOfWork())
                 .setId(suggestionDto.getId())
                 .setPrice(suggestionDto.getPrice())
-                .setSpecialist(suggestionDto.getSpecialist())
+                .setSpecialist(mapper.toSpecialist(suggestionDto.getSpecialistDto()))
                 .setStartTime(suggestionDto.getStartTime())
                 .setWorkDescription(suggestionDto.getWorkDescription());
     }
     SuggestionDto toSuggestionDto(Suggestion suggestion){
         return new SuggestionDto()
-                .setCustomerOrder(suggestion.getCustomerOrder())
+                .setCustomerOrder(mapper.toCustomerOrderDto(suggestion.getCustomerOrder()))
                 .setDurationOfWork(suggestion.getDurationOfWork())
                 .setId(suggestion.getId())
                 .setPrice(suggestion.getPrice())
-                .setSpecialist(suggestion.getSpecialist())
+                .setSpecialistDto(mapper.toSpecialistDto(suggestion.getSpecialist()))
                 .setStartTime(suggestion.getStartTime())
                 .setWorkDescription(suggestion.getWorkDescription());
     }
