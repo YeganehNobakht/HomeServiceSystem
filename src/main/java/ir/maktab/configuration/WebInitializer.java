7e60ff79389799db3d5cd0af840a5e76ca1c6ec1
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-08 13:06:35 +0430

    adding getServletFilters.

--- src/main/java/ir/maktab/configuration/WebInitializer.java
+++ src/main/java/ir/maktab/configuration/WebInitializer.java
@@ -1,11 +1,14 @@
 package ir.maktab.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-/**
- * @author Yeganeh Nobakht
- **/
+
+import javax.servlet.Filter;
+@Configuration
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
@@ -13,11 +16,20 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
+        //add app configuration as an array
         return new Class[]{AppConfiguration.class};
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] { characterEncodingFilter };
     }
 }
