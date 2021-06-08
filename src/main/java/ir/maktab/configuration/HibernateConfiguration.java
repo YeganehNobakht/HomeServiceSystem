ddf590bee065f89f9f5809c8546bd15ff100a686
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-08 12:53:33 +0430

    with spring data no need for HibernateConfiguration.java.

--- src/main/java/ir/maktab/configuration/HibernateConfiguration.java
+++ src/main/java/ir/maktab/configuration/HibernateConfiguration.java
@@ -1,58 +1,59 @@
-package ir.maktab.configuration;
-
-import ir.maktab.data.entity.*;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-
-import java.util.Properties;
-
-@Configuration
-@PropertySource("classpath:database.properties")
-public class HibernateConfiguration {
-
-    private final Environment environment;
-
-    public HibernateConfiguration(Environment environment) {
-        this.environment = environment;
-    }
-
-
-    @Bean("sessionFactory")
-    public SessionFactory getSessionFactory() {
-        try {
-            org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
-            configuration.addAnnotatedClass(Manager.class);
-            configuration.addAnnotatedClass(Specialist.class);
-            configuration.addAnnotatedClass(Customer.class);
-            configuration.addAnnotatedClass(CustomerOrder.class);
-            configuration.addAnnotatedClass(ServiceCategory.class);
-            configuration.addAnnotatedClass(SubCategory.class);
-            configuration.addAnnotatedClass(CustomerComment.class);
-            configuration.setProperties(new Properties() {
-                {
-                    put("hibernate.connection.driver_class", environment.getRequiredProperty("hibernate.connection.driver_class"));
-                    put("hibernate.connection.url", environment.getRequiredProperty("hibernate.connection.url"));
-                    put("hibernate.connection.username", environment.getRequiredProperty("hibernate.connection.username"));
-                    put("hibernate.connection.password", environment.getRequiredProperty("hibernate.connection.password"));
-                    put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-                    put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-                    put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-                    put("hibernate.connection.isolation", environment.getRequiredProperty("hibernate.connection.isolation"));
-                }
-
-            });
-
-            return configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-}
-
+//package ir.maktab.configuration;
+//
+//import ir.maktab.data.entity.*;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.core.env.Environment;
+//
+//import java.util.Properties;
+//
+//@Configuration
+//@PropertySource("classpath:database.properties")
+//public class HibernateConfiguration {
+//
+//    private final Environment environment;
+//
+//    public HibernateConfiguration(Environment environment) {
+//        this.environment = environment;
+//    }
+//
+//
+//    @Bean("sessionFactory")
+//    public SessionFactory getSessionFactory() {
+//        try {
+//            org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+//            configuration.addAnnotatedClass(Manager.class);
+//            configuration.addAnnotatedClass(Specialist.class);
+//            configuration.addAnnotatedClass(Customer.class);
+//            configuration.addAnnotatedClass(CustomerOrder.class);
+//            configuration.addAnnotatedClass(ServiceCategory.class);
+//            configuration.addAnnotatedClass(SubCategory.class);
+//            configuration.addAnnotatedClass(CustomerComment.class);
+//            configuration.addAnnotatedClass(Suggestion.class);
+//            configuration.setProperties(new Properties() {
+//                {
+//                    put("hibernate.connection.driver_class", environment.getRequiredProperty("hibernate.connection.driver_class"));
+//                    put("hibernate.connection.url", environment.getRequiredProperty("hibernate.connection.url"));
+//                    put("hibernate.connection.username", environment.getRequiredProperty("hibernate.connection.username"));
+//                    put("hibernate.connection.password", environment.getRequiredProperty("hibernate.connection.password"));
+//                    put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+//                    put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+//                    put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+//                   // put("hibernate.connection.isolation", environment.getRequiredProperty("hibernate.connection.isolation"));
+//                }
+//
+//            });
+//
+//            return configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
+//                    .applySettings(configuration.getProperties()).build());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//}
+//
