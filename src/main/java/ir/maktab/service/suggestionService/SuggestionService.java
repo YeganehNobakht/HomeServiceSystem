cf34a126c8ec61a7077ec7c6c1643522f556f3f8
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 16:14:47 +0430

    add some method to SuggestionService interface.

--- src/main/java/ir/maktab/service/suggestionService/SuggestionService.java
+++ src/main/java/ir/maktab/service/suggestionService/SuggestionService.java
@@ -1,6 +1,9 @@
 package ir.maktab.service.suggestionService;
 
-import ir.maktab.data.entity.User;
+import ir.maktab.data.entity.enums.OrderStatus;
+import ir.maktab.data.entity.enums.SuggestionStatus;
+import ir.maktab.dto.CustomerOrderDto;
+import ir.maktab.dto.SpecialistDto;
 import ir.maktab.dto.SuggestionDto;
 
 import java.util.List;
@@ -9,7 +12,14 @@ public interface SuggestionService {
 
     void addSuggestionForOrder(SuggestionDto suggestionDto);
 
-    public List<SuggestionDto> filterUser(SuggestionDto suggestionDto);
-    //void updateSuggestion(SuggestionDto suggestionDto);
+    public List<SuggestionDto> filterSuggestion(SuggestionDto suggestionDto);
+//    void updateSuggestion(SuggestionDto suggestionDto);
+    List<SuggestionDto> findByCustomerOrder(CustomerOrderDto customerOrderDto) throws Exception;
+    List<SuggestionDto> findBySpecialist(SpecialistDto specialist) ;
+    SuggestionDto findBySpecialist_IdAndOrderId(Integer specialistId,Integer orderId) ;
 
+    SuggestionDto findById(Integer id) throws Exception;
+
+    SuggestionDto update(SuggestionDto suggestionDto) ;
+    List<SuggestionDto> findUserBySuggestionStatusAndSpecialist(SuggestionStatus status, Integer id);
 }
