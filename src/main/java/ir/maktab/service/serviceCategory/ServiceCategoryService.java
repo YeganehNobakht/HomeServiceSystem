4d20821d9fe1ce21bdc453e5bebe41cce3de26ef
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:23:10 +0430

    adding some methods to ServiceCategoryService interface.

--- src/main/java/ir/maktab/service/serviceCategory/ServiceCategoryService.java
+++ src/main/java/ir/maktab/service/serviceCategory/ServiceCategoryService.java
@@ -3,18 +3,27 @@ package ir.maktab.service.serviceCategory;
 import ir.maktab.data.entity.ServiceCategory;
 import ir.maktab.dto.ServiceCategoryDto;
 import ir.maktab.dto.SpecialistDto;
-import ir.maktab.dto.SubCategoryDto;
 
 import java.util.List;
 
 public interface ServiceCategoryService {
     ServiceCategory get(Integer id) throws Exception;
-    ServiceCategory getByName(String name) throws Exception;
+
+    ServiceCategoryDto getByName(String name) throws Exception;
+
     void addServiceCategory(ServiceCategoryDto serviceCategoryDto) throws Exception;
+
     void updateSpecialist(SpecialistDto specialistDto) throws Exception;
-    void deleteSpecialist(ServiceCategoryDto serviceCategoryDto,SpecialistDto specialistDto) throws Exception;
-    void addSpecialist(ServiceCategoryDto  serviceCategoryDto , SpecialistDto specialistDto) throws Exception;
+
+    void deleteSpecialist(ServiceCategoryDto serviceCategoryDto, SpecialistDto specialistDto) throws Exception;
+
+    void addSpecialist(ServiceCategoryDto serviceCategoryDto, SpecialistDto specialistDto) throws Exception;
+
     void update(ServiceCategoryDto serviceCategoryDto) throws Exception;
+
     List<ServiceCategoryDto> getAll();
+
     void delete(ServiceCategoryDto serviceCategoryDto) throws Exception;
+
+    ServiceCategoryDto sava(ServiceCategoryDto serviceCategoryDto);
 }
