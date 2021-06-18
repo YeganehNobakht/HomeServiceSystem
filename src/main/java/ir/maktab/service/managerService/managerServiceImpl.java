86942a9defa7e8b417885a028861c45223664d99
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:15:28 +0430

    implement login method in managerServiceImpl.

--- src/main/java/ir/maktab/service/managerService/managerServiceImpl.java
+++ src/main/java/ir/maktab/service/managerService/managerServiceImpl.java
@@ -1,40 +1,43 @@
 package ir.maktab.service.managerService;
 
 import ir.maktab.data.entity.Customer;
-import ir.maktab.data.entity.ServiceCategory;
+import ir.maktab.data.entity.Manager;
 import ir.maktab.data.entity.Specialist;
-import ir.maktab.dto.CustomerDto;
-import ir.maktab.dto.ServiceCategoryDto;
-import ir.maktab.dto.SpecialistDto;
-import ir.maktab.dto.SubCategoryDto;
+import ir.maktab.data.repository.manager.ManagerRepository;
+import ir.maktab.dto.*;
 import ir.maktab.service.customerService.CustomerService;
+import ir.maktab.service.mapper.ManagerMapper;
 import ir.maktab.service.serviceCategory.ServiceCategoryService;
 import ir.maktab.service.specialistService.SpecialistService;
 import ir.maktab.service.validations.Validations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
 
 @Service
-public class managerServiceImpl implements ManagerService{
+public class managerServiceImpl implements ManagerService {
 
     private final Validations validations;
     private final CustomerService customerService;
     private final SpecialistService specialistService;
     private final ServiceCategoryService serviceCategoryService;
+    private final ManagerMapper managerMapper;
+    private final ManagerRepository managerRepository;
 
-    public managerServiceImpl(Validations validations, CustomerService customerService, SpecialistService specialistService, ServiceCategoryService serviceCategoryService) {
+    public managerServiceImpl(Validations validations, CustomerService customerService, SpecialistService specialistService, ServiceCategoryService serviceCategoryService, ManagerMapper managerMapper, ManagerRepository managerRepository) {
         this.validations = validations;
         this.customerService = customerService;
         this.specialistService = specialistService;
         this.serviceCategoryService = serviceCategoryService;
+        this.managerMapper = managerMapper;
+        this.managerRepository = managerRepository;
     }
 
 
     @Override
     public void addCustomer(CustomerDto customerDto) throws Exception {
-        Customer customer = new Customer(customerDto.getUsername(),customerDto.getPassword(),customerDto.getName(),customerDto.getLastName(),customerDto.getEmail());
+        Customer customer = new Customer(customerDto.getUsername(), customerDto.getPassword(), customerDto.getName(), customerDto.getLastName(), customerDto.getEmail());
         if (validations.validatePassword(customer.getPassword())
                 && validations.validateEmail(customer.getEmail())
                 && validations.validateName(customer.getName())
@@ -49,23 +52,24 @@ public class managerServiceImpl implements ManagerService{
     @Override
     public void addSpecialist(SpecialistDto specialistDto) throws Exception {
 
-        Specialist specialist = new Specialist(specialistDto.getUsername(),specialistDto.getPassword(),specialistDto.getName(),specialistDto.getLastName(),specialistDto.getEmail());
+        //TODO :: correct it
+        Specialist specialist = new Specialist(specialistDto.getUsername(), specialistDto.getPassword(), specialistDto.getName(), specialistDto.getLastName(), specialistDto.getEmail());
         if (validations.validatePassword(specialist.getPassword())
                 && validations.validateEmail(specialist.getEmail())
                 && validations.validateName(specialist.getName())
                 && validations.validateName(specialist.getLastName())
                 && validations.validateUsername(specialist.getUsername())) {
 
-            specialistService.create(specialistDto);
+            //specialistService.create(specialistDto);
         }
 
     }
 
     @Override
-    public void deleteSpecialistFromService(String serviceName, String specialistUsername) throws Exception {
-        ServiceCategory service = serviceCategoryService.getByName(serviceName);
-        if (service.getSpecialistList().stream().anyMatch(specialist -> specialist.getUsername().equals(specialistUsername)))
-            specialistService.delete(specialistUsername);
+    public void deleteSpecialistFromService(String serviceName, Integer id) throws Exception {
+        ServiceCategoryDto service = serviceCategoryService.getByName(serviceName);
+        if (service.getSpecialistList().stream().anyMatch(specialist -> specialist.getId().equals(id)))
+            specialistService.delete(id);
         else
             throw new Exception("Specialist not found");
 
@@ -99,6 +103,15 @@ public class managerServiceImpl implements ManagerService{
     @Override
     public List<SubCategoryDto> getSubCategory() {
         return null;
+    }
+
+    @Override
+    public ManagerDto login(ManagerDto managerDto) throws Exception {
+        Optional<Manager> manager = managerRepository.findByUsernameAndPassword(managerDto.getUsername(),managerDto.getPassword());
+        if (manager.isPresent()) {
+           return managerMapper.toManagerDto(manager.get());
+        }
+        throw new Exception("manager is not registered");
     }
 
 
