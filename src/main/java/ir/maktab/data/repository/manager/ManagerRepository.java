c4dd363ae248c18e77b05d0da72b26d193c69f6c
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:14:04 +0430

    adding findByUsernameAndPassword method to ManagerRepository.

--- src/main/java/ir/maktab/data/repository/manager/ManagerRepository.java
+++ src/main/java/ir/maktab/data/repository/manager/ManagerRepository.java
@@ -5,10 +5,11 @@ import ir.maktab.data.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+
 @Repository
-public interface ManagerRepository extends JpaRepository<Manager,String> {
+public interface ManagerRepository extends JpaRepository<Manager, String> {
+    Optional<Manager> findByUsernameAndPassword(String username, String password);
 //    void create(Manager manager);
 //    void update(Manager manager);
 //    Optional<Manager> get(Integer id);
