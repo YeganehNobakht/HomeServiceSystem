a0ce462e9844868741e58a686b5084ae58f0aa3e
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:14:47 +0430

    adding login method to ManagerService interface.

--- src/main/java/ir/maktab/service/managerService/ManagerService.java
+++ src/main/java/ir/maktab/service/managerService/ManagerService.java
@@ -1,22 +1,29 @@
 package ir.maktab.service.managerService;
 
 
-import ir.maktab.dto.CustomerDto;
-import ir.maktab.dto.ServiceCategoryDto;
-import ir.maktab.dto.SpecialistDto;
-import ir.maktab.dto.SubCategoryDto;
+import ir.maktab.dto.*;
 
 import java.util.List;
 
 public interface ManagerService {
     void addSpecialist(SpecialistDto specialistDto) throws Exception;
+
     void addCustomer(CustomerDto customerDto) throws Exception;
-    void deleteSpecialistFromService(String ServiceName, String SpecialistUsername) throws Exception;
+
+    void deleteSpecialistFromService(String ServiceName, Integer id) throws Exception;
+
     void editSpecialistFromService(String ServiceName, String SpecialistUsername);
+
     void addSpecialistToService();
+
     void addService();
+
     void addSubService();
+
     List<ServiceCategoryDto> getServiceCategory();
+
     List<SubCategoryDto> getSubCategory();
+
+    ManagerDto login(ManagerDto managerDto) throws Exception;
     //List<User>
 }
