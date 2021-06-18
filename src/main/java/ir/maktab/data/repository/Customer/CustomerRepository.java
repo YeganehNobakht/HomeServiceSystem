eae20918376130edf5e539129076ee171112187c
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 15:32:48 +0430

    adding update method.

--- src/main/java/ir/maktab/data/repository/Customer/CustomerRepository.java
+++ src/main/java/ir/maktab/data/repository/Customer/CustomerRepository.java
@@ -1,18 +1,26 @@
 package ir.maktab.data.repository.Customer;
 
 import ir.maktab.data.entity.Customer;
-import ir.maktab.dto.CustomerDto;
+import ir.maktab.data.entity.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,String> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 //    void create(Customer customer);
 //    void update(Customer customer);
 //    Optional<Customer> get(String  id);
 //    List<Customer> getAll();
 //    void delete(String integer);
 //    List<CustomerDto> filterCustomer(CustomerDto customerDto);
+    Optional<Customer> findByUsername(String username);
+
+    @Modifying
+    @Query("update Customer c set c.Balance = :balance where c.id = :id")
+    void updateBalance(@Param("id")Integer id,@Param("balance") double balance);
 }
