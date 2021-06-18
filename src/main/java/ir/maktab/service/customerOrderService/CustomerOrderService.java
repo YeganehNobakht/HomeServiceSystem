795284b878d4633d57230a18087cdc186601208c
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 15:42:02 +0430

    adding some methods to CustomerOrderService.

--- src/main/java/ir/maktab/service/customerOrderService/CustomerOrderService.java
+++ src/main/java/ir/maktab/service/customerOrderService/CustomerOrderService.java
@@ -1,12 +1,19 @@
 package ir.maktab.service.customerOrderService;
 
 import ir.maktab.data.entity.CustomerOrder;
-import ir.maktab.dto.CustomerDto;
-import ir.maktab.dto.CustomerOrderDto;
-import ir.maktab.dto.ServiceCategoryDto;
-import ir.maktab.dto.SubCategoryDto;
+import ir.maktab.data.entity.enums.OrderStatus;
+import ir.maktab.dto.*;
+
+import java.util.List;
 
 public interface CustomerOrderService {
-    void addOrder(CustomerOrderDto customerOrderDto);
+    void addOrder(OrderDto orderDto) throws Exception;
+
+    List<CustomerOrderDto> findByService(ServiceCategoryDto serviceCategoryDto) throws Exception;
+    CustomerOrderDto findById(Integer orderId) throws Exception;
+    List<CustomerOrderDto> findByCustomer(CustomerDto customerDto) throws Exception;
+    List<CustomerOrderDto> findByOrderStatusNotAndCustomer_Id(OrderStatus orderStatus , Integer customerId);
+    void updateOrderStatus(CustomerOrderDto orderDto) throws Exception;
+    List<CustomerOrderDto> findUserByStatusAndCustomer(OrderStatus orderStatus, CustomerDto customerDto) throws Exception;
 
 }
