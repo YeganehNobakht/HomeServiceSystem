ea26c7e17e33d4ef8b8b58434cd42a24cfdba294
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:43:30 +0430

    changing specification in UserRepository.

--- src/main/java/ir/maktab/data/repository/User/UserRepository.java
+++ src/main/java/ir/maktab/data/repository/User/UserRepository.java
@@ -1,50 +1,61 @@
 package ir.maktab.data.repository.User;
 
-import ir.maktab.data.entity.CustomerOrder;
+import com.mysql.cj.util.StringUtils;
+import ir.maktab.data.entity.Customer;
+import ir.maktab.data.entity.ServiceCategory;
+import ir.maktab.data.entity.Specialist;
 import ir.maktab.data.entity.User;
-import ir.maktab.dto.UserDto;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import ir.maktab.dto.UserFilter;
+import ir.maktab.dto.enums.UserRole;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,String>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
 
 //    List<UserDto> filterUser(UserDto userDto);
 
-    static Specification<User> filterUser(User user){
-        return (Specification<User>) (root, cq, cb) -> {
-            ArrayList<Predicate> predicates = new ArrayList<>();
+    static Specification<User> filterCustomers(UserFilter user, ServiceCategory serviceCategory) {
+        return (Specification<User>) (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
 
-            //join<>
-           // paganle.....sort(specialistRate)
+            CriteriaQuery<User> userCriteriaQuery = criteriaBuilder.createQuery(User.class);
+            Subquery<Specialist> specialistSubquery = userCriteriaQuery.subquery(Specialist.class);
+            Root<Specialist> specialistRoot = specialistSubquery.from(Specialist.class);
 
+//            if (user.getUserRole().equals(UserRole.Specialist)){
+//                userCriteriaQuery = criteriaBuilder.createQuery(Specialist.class);
+//            }
 
-//root:suggestion
-            //Join<suggestion, CustomerOrder> order = root.join("order");
-            //Join<suggestion, specialist> specialist = root.join("specialist");
+            if (user.getUserRole()!=null) {
+                predicates.add(criteriaBuilder.equal(root.get("userRole"), user.getUserRole()));
+            }
 
+            if (!StringUtils.isNullOrEmpty(user.getName())) {
+                predicates.add(criteriaBuilder.equal(root.get("name"), user.getName()));
+            }
 
-            // cq.orderBy(cb.desc(root.get("price")))
+            if (!StringUtils.isNullOrEmpty(user.getLastName())) {
+                predicates.add(criteriaBuilder.equal(root.get("lastName"), user.getLastName()));
+            }
 
-           // cq.orderBy(cb.desc(specialist.get("specialistRate")))
-            if (user.getName()!=null)
-                predicates.add(cb.equal(root.get("name"),user.getName()));
-            if (user.getLastName()!=null)
-                predicates.add(cb.equal(root.get("lastName"),user.getLastName()));
-            if (user.getUserRole()!=null)
-                predicates.add(cb.equal(root.get("userRole"),user.getUserRole()));
-            if (user.getEmail()!=null)
-                predicates.add(cb.equal(root.get("email"),user.getEmail()));
-            return cq.where(predicates.toArray(new Predicate[0])).getRestriction();
+            if (!StringUtils.isNullOrEmpty(user.getEmail())) {
+                predicates.add(criteriaBuilder.equal(root.get("email"), user.getEmail()));
+            }
+
+            if (serviceCategory != null) {
+                //if you have several conditions then you should create a new predicate and add the conditions to it like above then add this new predicate to where clause.
+                specialistSubquery.select(specialistRoot.get("id")).where(criteriaBuilder.isMember(serviceCategory,specialistRoot.get("serviceCategoryList")));
+                predicates.add(root.get("id").in(specialistSubquery));
+            }
+
+            return userCriteriaQuery.where(predicates.toArray(new Predicate[0])).getRestriction();
         };
     }
 }
