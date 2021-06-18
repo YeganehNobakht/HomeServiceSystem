b7916a15cf1847f71f37b7f6edb53495f1298798
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 15:33:20 +0430

    adding update method in service interface.

--- src/main/java/ir/maktab/service/customerService/CustomerService.java
+++ src/main/java/ir/maktab/service/customerService/CustomerService.java
@@ -1,19 +1,28 @@
 package ir.maktab.service.customerService;
 
 import ir.maktab.data.entity.Customer;
+import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.dto.CustomerDto;
-import ir.maktab.dto.CustomerLoginDto;
 import ir.maktab.dto.CustomerOrderDto;
+import ir.maktab.dto.OrderDto;
+
+import java.util.List;
 
 public interface CustomerService {
 
     Customer customerSignIn() throws Exception;
 
-    void addOrder(CustomerOrderDto customerDto) ;
+    void update(CustomerDto customerDto) throws Exception;
 
-    void changePassword(String username, String oldPass,String newPass) throws Exception;
+    void changePassword(String username, String oldPass, String newPass) throws Exception;
 
     void create(CustomerDto customerOrderDto) throws Exception;
 
     CustomerDto login(CustomerDto customerDto) throws Exception;
+
+    List<CustomerOrderDto> getOrders(CustomerDto customerDto) throws Exception;
+
+    double returnMoney(double price, CustomerDto customerDto);
+
+    void payByAccount(CustomerDto customerDto, double price);
 }
