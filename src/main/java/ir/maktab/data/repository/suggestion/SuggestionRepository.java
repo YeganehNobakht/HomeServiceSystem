0f0dbc7955cd40c4468d68e1446fed2cb02546fb
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 16:14:13 +0430

    add some method and query to SuggestionRepository.

--- src/main/java/ir/maktab/data/repository/suggestion/SuggestionRepository.java
+++ src/main/java/ir/maktab/data/repository/suggestion/SuggestionRepository.java
@@ -1,35 +1,54 @@
 package ir.maktab.data.repository.suggestion;
 
+import ir.maktab.data.entity.Customer;
 import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.entity.Specialist;
 import ir.maktab.data.entity.Suggestion;
-import ir.maktab.data.entity.User;
+import ir.maktab.data.entity.enums.OrderStatus;
+import ir.maktab.data.entity.enums.SuggestionStatus;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
-public interface SuggestionRepository extends JpaRepository<Suggestion,Long> , JpaSpecificationExecutor<Suggestion> {
+public interface SuggestionRepository extends JpaRepository<Suggestion, Integer>, JpaSpecificationExecutor<Suggestion> {
 
 
-    static Specification<Suggestion> filterSuggestion(Suggestion suggestion){
+    List<Suggestion> findBySpecialist(Specialist specialist);
+    Suggestion findBySpecialist_IdAndAndCustomerOrder_Id(Integer specialistId,Integer orderId);
+
+    List<Suggestion> findByCustomerOrder_Id(Integer order);
+
+    @Query("SELECT s FROM Suggestion s WHERE s.suggestionStatus = :status and s.specialist.id = :specialistId")
+    List<Suggestion> findUserBySuggestionStatusAndSpecialist(@Param("status") SuggestionStatus status, @Param("specialistId") Integer id);
+
+
+    static Specification<Suggestion> filterSuggestion(Suggestion suggestion) {
         return (Specification<Suggestion>) (root, cq, cb) -> {
             ArrayList<Predicate> predicates = new ArrayList<>();
 
-            Join<Suggestion, CustomerOrder> order = root.join("customerOrder");
+            if (suggestion.getCustomerOrder()!=null){
+                predicates.add(cb.equal(root.get("customerOrder"), suggestion.getCustomerOrder()));
+            }
+            if (suggestion.getPrice()!=null){
+                predicates.add(cb.le(root.get("price"),suggestion.getPrice()));
+            }
             Join<Suggestion, Specialist> specialist = root.join("specialist");
 
+            if (suggestion.getSpecialist().getRate()!=0){
+                predicates.add(cb.ge(specialist.get("rate"),suggestion.getSpecialist().getRate()));
+            }
 
-
-//             cq.orderBy(cb.desc(root.get("price")),);
-//             cq.orderBy(cb.desc(specialist.get("rate")));
-
-            cq.orderBy(cb.desc(root.get("price")),cb.desc(specialist.get("rate")));
+            cq.orderBy(cb.desc(root.get("price")), cb.desc(specialist.get("rate")));
 
             return cq.where(predicates.toArray(new Predicate[0])).getRestriction();
         };
