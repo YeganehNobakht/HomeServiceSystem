7c6318f588b8921491d0b2c760554323e9c37e34
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 17:22:21 +0430

    changing id type in ServiceCategoryRepository.

--- src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepository.java
+++ src/main/java/ir/maktab/data/repository/serviceCategory/ServiceCategoryRepository.java
@@ -1,15 +1,12 @@
 package ir.maktab.data.repository.serviceCategory;
 
-import ir.maktab.data.entity.CustomerOrder;
 import ir.maktab.data.entity.ServiceCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 @Repository
-public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory,Integer> {
-//    void create(ServiceCategory serviceCategory);
+public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory, Integer> {
+    //    void create(ServiceCategory serviceCategory);
 //    void update(ServiceCategory serviceCategory);
 //    Optional<ServiceCategory> get(Integer id);
 //    Optional<ServiceCategory> getByName(String name);
