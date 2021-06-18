176cae1045d44e03a64d8722ba1a37fd18961ed9
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 16:06:16 +0430

    adding join table in specialist entity.

--- src/main/java/ir/maktab/data/entity/Specialist.java
+++ src/main/java/ir/maktab/data/entity/Specialist.java
@@ -1,7 +1,6 @@
 package ir.maktab.data.entity;
 
 import ir.maktab.data.entity.enums.UserStatus;
-import ir.maktab.dto.SpecialistDto;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -9,55 +8,62 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Specialist extends User{
-
-    private Double rate;
-
+public class Specialist extends User {
+    private double rate;
     @Lob
-    @Column(length = 300000,columnDefinition = "BLOB")
+    @Column(length = 300000, columnDefinition = "BLOB")
     @Basic(fetch = FetchType.LAZY)
     private byte[] profilePicture;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "specialist_service",
+            joinColumns = {@JoinColumn(name = "specialist_id")},
+            inverseJoinColumns = {@JoinColumn(name = "service_id")}
+    )
     private List<ServiceCategory> serviceCategoryList = new ArrayList<>();
 
-    @OneToMany(orphanRemoval = true , cascade = CascadeType.PERSIST , mappedBy = "specialist")
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST, mappedBy = "specialist")
     private List<CustomerComment> customerCommentList = new ArrayList<>();
 
-    @OneToMany(orphanRemoval = true , cascade = CascadeType.PERSIST , mappedBy = "specialist")
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST, mappedBy = "specialist")
     private List<Suggestion> suggestionList = new ArrayList<>();
 
-@OneToMany(orphanRemoval = true , cascade = CascadeType.PERSIST , mappedBy = "specialist")
-private List<CustomerOrder> customerOrderList = new ArrayList<>();
-   //constructor
-
+//    @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST, mappedBy = "specialist")
+//    private List<CustomerOrder> customerOrderList = new ArrayList<>();
 
     public Specialist(String username, String password, String name, String lastName, String email) {
         super(username, password, name, lastName, email);
     }
 
-    public Specialist(){
+    public Specialist() {
 
     }
 
 
-    public Double getRate() {
+    @Override
+    public Specialist setId(Integer id) {
+        super.setId(id);
+        return this;
+    }
+
+    public double getRate() {
         return rate;
     }
 
-    public Specialist setRate(Double rate) {
+    public Specialist setRate(double rate) {
         this.rate = rate;
         return this;
     }
 
-    public List<CustomerOrder> getCustomerOrderList() {
-        return customerOrderList;
-    }
-
-    public Specialist setCustomerOrderList(List<CustomerOrder> customerOrderList) {
-        this.customerOrderList = customerOrderList;
-        return this;
-    }
+//    public List<CustomerOrder> getCustomerOrderList() {
+//        return customerOrderList;
+//    }
+//
+//    public Specialist setCustomerOrderList(List<CustomerOrder> customerOrderList) {
+//        this.customerOrderList = customerOrderList;
+//        return this;
+//    }
 
     public byte[] getProfilePicture() {
         return profilePicture;
@@ -72,8 +78,18 @@ private List<CustomerOrder> customerOrderList = new ArrayList<>();
         return serviceCategoryList;
     }
 
+    public Specialist setServiceCategoryList(List<ServiceCategory> serviceCategoryList) {
+        this.serviceCategoryList = serviceCategoryList;
+        return this;
+    }
+
     public List<CustomerComment> getCustomerCommentList() {
         return customerCommentList;
+    }
+
+    public Specialist setCustomerCommentList(List<CustomerComment> customerCommentList) {
+        this.customerCommentList = customerCommentList;
+        return this;
     }
 
     @Override
@@ -121,16 +137,6 @@ private List<CustomerOrder> customerOrderList = new ArrayList<>();
     @Override
     public Specialist setBalance(Double balance) {
         super.setBalance(balance);
-        return this;
-    }
-
-    public Specialist setServiceCategoryList(List<ServiceCategory> serviceCategoryList) {
-        this.serviceCategoryList = serviceCategoryList;
-        return this;
-    }
-
-    public Specialist setCustomerCommentList(List<CustomerComment> customerCommentList) {
-        this.customerCommentList = customerCommentList;
         return this;
     }
 
