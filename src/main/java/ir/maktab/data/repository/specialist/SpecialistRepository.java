6ffe4e87bd63c2c3bc93d79e6aa305cd93c023db
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 16:03:08 +0430

    fixing id type and adding some methods to SpecialistRepository.

--- src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
+++ src/main/java/ir/maktab/data/repository/specialist/SpecialistRepository.java
@@ -1,19 +1,23 @@
 package ir.maktab.data.repository.specialist;
 
-import ir.maktab.data.entity.Customer;
 import ir.maktab.data.entity.Specialist;
-import ir.maktab.dto.SpecialistDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+
 @Repository
-public interface SpecialistRepository extends JpaRepository<Specialist,String> {
+public interface SpecialistRepository extends JpaRepository<Specialist, Integer> {
 //    void create(Specialist specialist);
 //    void update(Specialist specialist);
 //    Optional<Specialist> get(String id);
 //    List<Specialist> getAll();
 //    void delete(String  username);
 //    List<SpecialistDto> filterSpecialist(SpecialistDto specialistDto);
+
+
+
+    Optional<Specialist> findByEmailAndUsername(String email,String username);
+
+    Optional<Specialist> findByUsernameAndPassword(String username, String Password);
 }
