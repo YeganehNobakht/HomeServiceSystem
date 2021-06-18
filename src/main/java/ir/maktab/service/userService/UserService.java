168f721715eff284ee28d064a32e450228ae6a5e
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-18 22:04:00 +0430

    adding filter method to UserService interface.

--- src/main/java/ir/maktab/service/userService/UserService.java
+++ src/main/java/ir/maktab/service/userService/UserService.java
@@ -1,9 +1,14 @@
 package ir.maktab.service.userService;
 
 import ir.maktab.data.entity.User;
+import ir.maktab.dto.UserDto;
+import ir.maktab.dto.UserFilter;
 
 import java.util.List;
 
 public interface UserService {
-    List<User> filterUser(User user);
+
+    List<UserDto> filterUser(UserFilter user) throws Exception;
+
+    List<UserDto> findAll();
 }
