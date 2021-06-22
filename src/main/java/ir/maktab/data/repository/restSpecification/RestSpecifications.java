1c364292405f448d294630b48b3f0554b7a41f17
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-22 11:19:13 +0430

    adding specification for spring rest.

---
+++ src/main/java/ir/maktab/data/repository/restSpecification/RestSpecifications.java
@@ -0,0 +1,124 @@
+package ir.maktab.data.repository.restSpecification;
+
+import com.mysql.cj.util.StringUtils;
+import ir.maktab.data.entity.*;
+import ir.maktab.data.entity.enums.SuggestionStatus;
+import ir.maktab.dto.enums.UserRole;
+import ir.maktab.dto.restDto.OrderFilterDto;
+import ir.maktab.dto.restDto.SuggestionFilterDto;
+import ir.maktab.dto.restDto.UserReportDto;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface RestSpecifications {
+
+    static Specification<Suggestion> filterSuggestion(SuggestionFilterDto dto, UserRole userRole){
+        return (Specification<Suggestion>) (root, query, criteriaBuilder) -> {
+            CriteriaQuery<Suggestion> suggestionQuery = criteriaBuilder.createQuery(Suggestion.class);
+            List<Predicate> predicateList = new ArrayList<>();
+            Join<Suggestion, Specialist> specialistJoin = root.join("specialist");
+            Join<Suggestion, CustomerOrder> customerOrderJoin = root.join("customerOrder");
+            Join<CustomerOrder, Customer> customerJoin = customerOrderJoin.join("customer");
+
+            predicateList.add(criteriaBuilder.equal(root.get("suggestionStatus"), SuggestionStatus.ACCEPTED));
+
+            if (userRole.equals(UserRole.Specialist)){
+                predicateList.add(criteriaBuilder.equal(specialistJoin.get("id"),dto.getId()));
+            }
+            if (userRole.equals(UserRole.Customer)){
+                predicateList.add(criteriaBuilder.equal(customerJoin.get("id"),dto.getId()));
+            }
+           if (dto.getSuggestionStatus()!=null)
+                predicateList.add(criteriaBuilder.equal(root.get("suggestionStatus"),dto.getSuggestionStatus()));
+            if (dto.getMaxSuggestionPrice()!=null)
+                predicateList.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"),dto.getMaxSuggestionPrice()));
+            if (dto.getMinSuggestionPrice()!=null)
+                predicateList.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"),dto.getMinSuggestionPrice()));
+            if (dto.getStartDate()!=null)
+                predicateList.add(criteriaBuilder.greaterThanOrEqualTo(root.get("startTime"),dto.getStartDate()));
+            if (dto.getEndDate()!=null)
+                predicateList.add(criteriaBuilder.lessThanOrEqualTo(root.get("startTime"),dto.getEndDate()));
+
+            return suggestionQuery.where(predicateList.toArray(new Predicate[0])).getRestriction();
+        };
+    }
+
+    static Specification<CustomerOrder> filterOrders(OrderFilterDto dto){
+        return (Specification<CustomerOrder>) (root, query, criteriaBuilder) -> {
+            CriteriaQuery<CustomerOrder> criteriaQuery = criteriaBuilder.createQuery(CustomerOrder.class);
+            List<Predicate> predicateList = new ArrayList<>();
+            Join<CustomerOrder, SubCategory> subCategoryJoin = root.join("subCategory");
+            Join<CustomerOrder, ServiceCategory> serviceCategoryJoin = root.join("serviceCategory");
+
+            if (dto.getOrderStatus() != null) {
+                predicateList.add(criteriaBuilder.equal(root.get("orderStatus"), dto.getOrderStatus()));
+            }
+            if (!StringUtils.isNullOrEmpty(dto.getSubServiceName())) {
+                predicateList.add(criteriaBuilder.equal(subCategoryJoin.get("name"), dto.getSubServiceName()));
+            }
+            if (!StringUtils.isNullOrEmpty(dto.getServiceName())) {
+                predicateList.add(criteriaBuilder.equal(serviceCategoryJoin.get("name"), dto.getServiceName()));
+            }
+            if (dto.getStartDate() != null) {
+                predicateList.add(criteriaBuilder.greaterThanOrEqualTo(root.get("orderDate"), dto.getStartDate()));
+            }
+            if (dto.getEndDate() != null) {
+                predicateList.add(criteriaBuilder.lessThanOrEqualTo(root.get("orderDate"), dto.getEndDate()));
+            }
+            return criteriaQuery.where(predicateList.toArray(new Predicate[0])).getRestriction();
+
+        };
+    }
+
+    static Specification<User> userReportFilter(UserReportDto dto){
+        return (root, query, criteriaBuilder) ->{
+            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+            List<Predicate> predicates=new ArrayList<>();
+
+            Subquery<Customer> customerSubquery = criteriaQuery.subquery(Customer.class);
+            Root<Customer> customerRoot = customerSubquery.from(Customer.class);
+            Subquery<Customer> customerSelect = customerSubquery.select(customerRoot.get("id"));
+            Predicate customerPredicate = criteriaBuilder.isTrue(criteriaBuilder.literal(true));
+
+
+
+            Subquery<Specialist> specialistSubquery = criteriaQuery.subquery(Specialist.class);
+            Root<Specialist> specialistRoot = specialistSubquery.from(Specialist.class);
+            Subquery<Specialist> expertSelect = specialistSubquery.select(specialistRoot.get("id"));
+            Predicate expertPredicate = criteriaBuilder.isTrue(criteriaBuilder.literal(true));
+
+
+            if (dto.getMaxNumOrders()!=null){
+                customerPredicate=criteriaBuilder.and(customerPredicate,criteriaBuilder.lessThanOrEqualTo(criteriaBuilder.size(customerRoot.get("customerOrderList")),dto.getMaxNumOrders()));
+            }
+            if (dto.getMinNumOrders()!=null){
+                customerPredicate=criteriaBuilder.and(customerPredicate,criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.size(customerRoot.get("customerOrderList")),dto.getMinNumOrders()));
+            }
+
+            if (dto.getMaxNumSuggestion()!=null){
+                expertPredicate=criteriaBuilder.and(expertPredicate,criteriaBuilder.lessThanOrEqualTo(criteriaBuilder.size(specialistRoot.get("suggestionList")), dto.getMaxNumSuggestion()));
+            }
+            if (dto.getMinNumSuggestion() != null) {
+                expertPredicate=criteriaBuilder.and(expertPredicate,criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.size(specialistRoot.get("suggestionList")), dto.getMinNumSuggestion()));
+            }
+            if (dto.getStartDate()!=null){
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("date"),dto.getStartDate()));
+            }
+            if (dto.getEndDate()!=null){
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("date"),dto.getEndDate()));
+            }
+            if (customerPredicate.getExpressions().size()>0){
+                customerSelect.where(customerPredicate);
+                predicates.add(root.get("id").in(customerSubquery));
+            }
+            if (expertPredicate.getExpressions().size()>0){
+                expertSelect.where(expertPredicate);
+                predicates.add(root.get("id").in(specialistSubquery));
+            }
+            return criteriaQuery.select(root).where(predicates.toArray(new Predicate[0])).getRestriction();
+        };
+    }
+}
