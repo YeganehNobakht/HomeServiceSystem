4299895957007c61ae01384170744204d55b20c2
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-06-25 17:29:41 +0430

    adding multipartResolver to AppConfiguration.

--- src/main/java/ir/maktab/configuration/AppConfiguration.java
+++ src/main/java/ir/maktab/configuration/AppConfiguration.java
@@ -1,6 +1,9 @@
 package ir.maktab.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Scanner;
@@ -13,9 +16,25 @@ import java.util.Scanner;
 @EnableWebMvc
 public class AppConfiguration {
 
+    @Bean("messageSource")
+    public MessageSource getMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
     @Bean("scanner")
     public Scanner scanner() {
         return new Scanner(System.in);
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        //set max upload size per file is 300KB
+        commonsMultipartResolver.setMaxUploadSizePerFile(300 * 1024);
+        return commonsMultipartResolver;
     }
 
 }
